91ff1dbd1874d3f897a0ccaffb609b10efbb5ed0
Ivan.Orlov <ivan@morrr.com>
2016-04-27 11:12:37 +0300

    Removing bower_components

--- bower_components/async-watch/app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-app.use('/', express.static(__dirname + '/src'));
-var port = process.env.PORT || 3022;
-var server = app.listen(port, function() {
-   var host = server.address().address;
-   console.log('Example app listening at http://%s:%s', host, port);
-});
